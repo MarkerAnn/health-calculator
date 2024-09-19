67c6273bac5327e5f1ab8be124a0bfc0292adcbe
Angelica Marker <am225cs@student.lnu.se>
2024-09-19 23:21:02 +0200

    Add interface for HealthCalculation class

---
+++ src/interfaces/InterfaceHealthCalculation.ts
@@ -0,0 +1,13 @@
+// TODO: Lägg alla funktioner i detta interface. Ta bort denna kommentar sedan.
+
+export interface InterfaceHealthCalculation {
+  calculateBmi: number
+  calculateBmiType: string
+  calculateBmiPrime: number
+  calculateBmr: number
+  calculateTdee: number
+  calculateIdealWeight: number
+  calculateBodyFatPercentage: number
+  calculateWaistToHipRatio: number // Lägg till typ här också?
+  calculateWaistToHeightRatio: number
+}
