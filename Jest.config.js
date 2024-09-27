10bb04e984adc67bd7268ca4f40a28f2a609b384
Angelica Marker <am225cs@student.lnu.se>
2024-09-27 15:26:03 +0200

    continue debugging jest in github actions

--- Jest.config.js
+++ Jest.config.js
@@ -3,4 +3,8 @@ module.exports = {
   testEnvironment: 'node',
   moduleFileExtensions: ['ts', 'js'],
   testMatch: ['**/*.test.ts'],
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
 }
