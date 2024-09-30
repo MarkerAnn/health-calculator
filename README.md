3205b6f6166c800822ef3f54e1228c36fa702896
Angelica Marker <am225cs@student.lnu.se>
2024-09-30 15:27:23 +0200

    Update .npmignore

--- README.md
+++ README.md
@@ -45,7 +45,14 @@ const healthCalculator = HealthCalculatorFactory.createHealthCalculator(user)
 
 console.log('BMI:', healthCalculator.getBmi())
 console.log('BMI Type:', healthCalculator.getBmiType())
+console.log('BMI Prime:', healthCalculator.getBmiPrime())
+console.log('Ideal Weight Range:', healthCalculator.getIdealWeight())
+console.log('Waist-to-Hip Ratio:', healthCalculator.getWaistToHipRatio())
+console.log('Waist-to-Height Ratio:', healthCalculator.getWaistToHeightRatio())
 console.log('Body Fat Percentage:', healthCalculator.getBodyFatPercentage())
+console.log('BMR (Harris-Benedict):', healthCalculator.getBmrHarrisBenedict())
+console.log('BMR (Mifflin-St Jeor):', healthCalculator.getBmrMifflinStJeor())
+console.log('TDEE (Harris-Benedict):', healthCalculator.getTdeeHarrisBenedict())
 console.log('TDEE (Mifflin-St Jeor):', healthCalculator.getTdeeMifflinStJeor())
 ```
 
--- src/app.ts
+++ src/app.ts
@@ -1,82 +1,30 @@
 import { HealthCalculatorFactory } from './factories/HealthCalculatorFactory'
 import { User } from './models/User'
 
-const underweightMaleImperial: User = {
-  weight: 121, // lbs
-  height: 5.9, // feet
-  age: 25,
-  gender: 'male',
-  waist: 29.5, // inches
-  hip: 35.4, // inches
-  neck: 13.8, // inches
-  unitSystem: 'imperial',
-  activityLevel: 'very',
-}
-
-const overweightFemaleMetric: User = {
-  weight: 75,
-  height: 1.65,
-  age: 35,
+const user: User = {
+  weight: 81, // kg
+  height: 1.63, // meters
+  age: 36,
   gender: 'female',
-  waist: 85,
-  hip: 100,
-  neck: 34,
+  waist: 92.5, // cm
+  hip: 110, // cm
+  neck: 36.5, // cm
   unitSystem: 'metric',
-  activityLevel: 'sedentary',
+  activityLevel: 'moderately',
 }
 
-const healthCalc = HealthCalculatorFactory.createHealthCalculator(
-  underweightMaleImperial
-)
+const healthCalculator = HealthCalculatorFactory.createHealthCalculator(user)
 
-console.log(
-  'BmiPrime',
-  healthCalc.getBmi(),
-  healthCalc.getBmiType(),
-  healthCalc.getBodyFatPercentage()
-)
-
-const heightInCm = 165
-const waistHipNeckSum = 85 + 100 - 34
-
-const heightFactor = 97.684 * Math.log10(heightInCm)
-const waistHipNeckFactor = 163.205 * Math.log10(waistHipNeckSum)
-const constantFactor = 78.387
-
-const bodyFatPercentage = waistHipNeckFactor - heightFactor - constantFactor
-
-console.log(
-  `Manuellt beräknad kroppsfettsprocent: ${bodyFatPercentage.toFixed(2)}%`
-)
-
-const calcOverweight = HealthCalculatorFactory.createHealthCalculator(
-  overweightFemaleMetric
-)
-console.log(
-  `Fabriksmetod beräknad kroppsfettsprocent: ${calcOverweight
-    .getBodyFatPercentage()
-    .toFixed(2)}%`
-)
-
-const normalWeightFemaleMetric: User = {
-  weight: 60,
-  height: 1.65,
-  age: 30,
-  gender: 'female',
-  waist: 70,
-  hip: 90,
-  neck: 32,
-  unitSystem: 'metric',
-  activityLevel: 'sedentary',
-}
-
-const calcNormalWeight = HealthCalculatorFactory.createHealthCalculator(
-  normalWeightFemaleMetric
-)
-
-console.log(
-  'Fat percentage for normal weight female:',
-  calcNormalWeight.getBodyFatPercentage().toFixed(2) + '%'
-)
+console.log('BMI:', healthCalculator.getBmi())
+console.log('BMI Type:', healthCalculator.getBmiType())
+console.log('BMI Prime:', healthCalculator.getBmiPrime())
+console.log('Ideal Weight Range:', healthCalculator.getIdealWeight())
+console.log('Waist-to-Hip Ratio:', healthCalculator.getWaistToHipRatio())
+console.log('Waist-to-Height Ratio:', healthCalculator.getWaistToHeightRatio())
+console.log('Body Fat Percentage:', healthCalculator.getBodyFatPercentage())
+console.log('BMR (Harris-Benedict):', healthCalculator.getBmrHarrisBenedict())
+console.log('BMR (Mifflin-St Jeor):', healthCalculator.getBmrMifflinStJeor())
+console.log('TDEE (Harris-Benedict):', healthCalculator.getTdeeHarrisBenedict())
+console.log('TDEE (Mifflin-St Jeor):', healthCalculator.getTdeeMifflinStJeor())
 
 //TODO: Ta bort denna
